5159cb074c8ee3602a3db991e1e80a4805508505
sapla1125 <sapla1125@gmail.com>
2022-01-23 00:15:38 -0500

    User Auth and Creatingg Pages with Navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { View, Text, Button} from 'react-native';
+
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+    return(
+        <View >
+            <Text>Home Screen</Text>
+            <Button title="Go to Chat Screen" onPress={() => navigation.navigate('Chat') }/>
+        </View>
+    )
+}
+
+export default HomeScreen
